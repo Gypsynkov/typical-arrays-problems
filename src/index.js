0e472f65c3e88bc4f97635af18db975036bedd84
Gypsynkov <mambakla@gmail.com>
2020-02-25 21:14:57 +0300

    apply

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+    let arr = [];
+    if (typeof array!== 'undefined' && array.length > 0){
+        return Math.min.apply( Math, array );
+    }else{
+    
+   
   return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
-}
-
+    if (typeof array!== 'undefined' && array.length > 0){
+    return Math.max.apply( Math, array );
+   }else{
+    
+   
+    return 0;}
+      }
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array!== 'undefined' && array.length > 0){
+    let average =  array.reduce( ( p, c ) => p + c, 0 ) / array.length;
+    return average;}
+    else { return 0}
 }
